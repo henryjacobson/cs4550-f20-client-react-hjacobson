025a8ba1f1ca3b241f673f72d3e03e8308b0e777
Henry <henryajacobson@gmail.com>
2020-11-09 14:38:43 -0600

    changed link

--- src/services/WidgetService.js
+++ src/services/WidgetService.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:8080/api"
+const url = "https://rocky-sierra-12080.herokuapp.com/api" // "http://localhost:8080/api"
 
 export const createWidget = (topicId, widget) =>
     fetch(`${url}/widgets`, {
