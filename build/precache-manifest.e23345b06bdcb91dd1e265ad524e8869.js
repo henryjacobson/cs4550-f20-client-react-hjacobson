7e9c7d33471bc10d795c5d8eba72ce9189873e84
Henry <henryajacobson@gmail.com>
2020-11-09 13:58:29 -0600

    deploy info

---
+++ build/precache-manifest.e23345b06bdcb91dd1e265ad524e8869.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8192af1f9561d5394b1b5f8e096da6fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecf509b8738b3473243",
+    "url": "/static/css/2.54dfa31c.chunk.css"
+  },
+  {
+    "revision": "dad95ad5bc7a223b3e9c",
+    "url": "/static/css/main.b7c445de.chunk.css"
+  },
+  {
+    "revision": "0ecf509b8738b3473243",
+    "url": "/static/js/2.d2d28370.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.d2d28370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad95ad5bc7a223b3e9c",
+    "url": "/static/js/main.ff28c1fe.chunk.js"
+  },
+  {
+    "revision": "74cee06cf19d87f9ff0a",
+    "url": "/static/js/runtime-main.57c81582.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
